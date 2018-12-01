e4d254552434a1769b09259d4c292456fa12ffe0
sraaphorst <sraaphorst@gemini.edu>
2018-12-01 02:59:58 -0300

    Adding comment.

--- day01/day011.py
+++ day01/day011.py
@@ -2,6 +2,11 @@
 
 
 def frequency(freqfile):
+    """
+    Given a file of frequencies of the form [+-]\d+, with one per line, find their sum.
+    :param freqfile: the name of the input file
+    :return: the sum of the frequencies
+    """
     return sum(map(int, open(freqfile).read().split('\n')))
 
 
